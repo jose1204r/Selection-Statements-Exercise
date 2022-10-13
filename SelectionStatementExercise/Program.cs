127e2417642e95dd3e99e48d2d3e717d6671d15b
jose <jrodriguez@itsolutionspcllc.com>
2022-10-12 21:57:36 -0400

    Selection Stament Execerse

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            // ******************** Exercise Selection Statement *********************************
+
+
+            Console.WriteLine("******** Welcome to Ramdon Game********* ");
+            Console.WriteLine("Try if can guess the correct number!");
+            Console.WriteLine("What is the upper limit for the rang");
+
+            Console.WriteLine(" PLEASE ENTER THE NUMBER : ");
+
+            var userResponse = int.Parse(Console.ReadLine());
+            var userpick = (userResponse);
+
+            var r = new Random();
+            var favoriteNumber = r.Next(1,userpick);
+           
+
+            if (userResponse > favoriteNumber)
+            {
+                Console.WriteLine("Hi Player! too high ");
+
+            }
+
+            else if (userResponse < favoriteNumber) {
+                Console.WriteLine("opp! Player too low ");
+
+            }
+            else 
+                    {
+                Console.WriteLine("*** Great! Player you got the rigth numner ****");
+            }
+
+            
         }
     }
 }
